5a61d38f5d6f1033c363a6ae58b95e0b3124f816
Gabriel Luiz Suzin Scolaro <demonskiller777@outlook.com>
2021-05-04 17:02:58 -0300

    nova pica

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,6 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Cancer />
-        </div>
-    )
-}
-function Cancer() {
-    const [cancer,setCancer] = useState(1);
-
-    function adicionarCancer() {
-        setCancer(cancer + cancer*2);
-    }
-    return (
-        <div>
-            <div>{cancer}</div>
-            <button onClick={adicionarCancer}>Adicionar</button>
         </div>
     )
 }
